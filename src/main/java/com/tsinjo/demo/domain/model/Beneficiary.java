b64519b5329d5f44a7fc19bbf9506202f6d88afe
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-08-06 16:46:12 +0300

    feat: Add new java class

--- src/main/java/com/tsinjo/demo/domain/model/Beneficiary.java
+++
@@ -1,3 +0,0 @@
-package com.tsinjo.demo.domain.model;
-
-public class Beneficiary {}
---
+++ src/main/java/com/tsinjo/demo/domain/model/Receiver.java
@@ -0,0 +1,14 @@
+package com.tsinjo.demo.domain.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Receiver {
+    private String fullName;
+    private String email;
+}
